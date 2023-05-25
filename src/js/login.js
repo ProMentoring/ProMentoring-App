0cdec0a4d3a30855a79580bbb4621c742fcb5648
Felipe <u202110902@upc.edu.pe>
2023-05-25 15:56:43 -0500

    feat: Login Responsive, API users y update del gitignore para los modulos de node

---
+++ src/js/login.js
@@ -0,0 +1,40 @@
+// Variables globales
+
+const user = document.getElementById("user");
+const contrasena = document.getElementById("contrasena");
+
+let users = [];
+
+// Funcion para obtener los usuarios de una API
+
+const getUsers = () => {
+     fetch('http://localhost:3000/users')
+          .then((response) => response.json())
+          .then((data) => {
+               users = data;
+          })
+          .catch((error) => alert(error));
+}
+
+// Capturar el formulario y enviar el id del usuario si existe
+document.getElementById("form-login").addEventListener("submit", function (event) {
+     event.preventDefault(); // Evitar que se envíe el formulario por defecto
+
+     // Validar si el usuario existe
+     const username = user.value;
+     const password = contrasena.value;
+
+     const foundUser = users.find((u) => (u.user === username || u.email === username) && u.password === password);
+
+     if (foundUser) {
+          // Redirigir a otra página con los datos incluidos en la URL
+          let url = "profile.html" + "?user=" + encodeURIComponent(username)
+               + "&contrasena=" + encodeURIComponent(password);
+          window.location.href = url;
+     } else {
+          alert("Usuario y/o contraseña incorrectos");
+     }
+});
+// MAIN
+
+getUsers();
