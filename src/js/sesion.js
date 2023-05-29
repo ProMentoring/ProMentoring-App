c45b7e8b6cb2db8a46eeb08073484caf1cbf0da3
Felipe <u202110902@upc.edu.pe>
2023-05-29 18:25:14 -0500

    refactor: Se cambiaron enlaces y se agrego el archivo js de sesion

---
+++ src/js/sesion.js
@@ -0,0 +1,18 @@
+// Variables para el link a cada pagina
+const busquedaLink = document.getElementById('busqueda-link');
+const profileLink = document.getElementById('profile-link');
+
+// Leer los valores de usuario de la URL
+let urlParams = new URLSearchParams(window.location.search);
+let user = urlParams.get("user");
+let type = urlParams.get("rol");
+console.log("Usuario: " + user);
+console.log("Type: " + type);
+
+// Funcion de enviar data a la pagina de busqueda
+busquedaLink.href = "busqueda.html" + "?user=" + encodeURIComponent(user)
+     + "&rol=" + encodeURIComponent(type);
+
+// Funcion de enviar data a la pagina sesion
+profileLink.href = "profile.html" + "?user=" + encodeURIComponent(user)
+     + "&rol=" + encodeURIComponent(type);
